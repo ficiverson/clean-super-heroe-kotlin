89c3c4351108fb6220f948a534f100e5e3e19c5a
Rodrigo Tugores <rodrigo.tugores@accenture.com>
2018-08-28 17:42:28 +0200

    Removed unused imports.

--- app-ui/src/androidTest/java/test/kotlin/clean/ficiverson/cleansh/SuperHeroeTest.kt
+++ app-ui/src/androidTest/java/test/kotlin/clean/ficiverson/cleansh/SuperHeroeTest.kt
@@ -1,9 +1,5 @@
 package test.kotlin.clean.ficiverson.cleansh
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
-import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
