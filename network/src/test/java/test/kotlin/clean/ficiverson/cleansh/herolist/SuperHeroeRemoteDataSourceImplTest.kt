21e81c72706c1748740143bb58c3313f680ca50c
Rodrigo Tugores <rodrigo.tugores@accenture.com>
2018-08-29 13:29:13 +0200

    Fixed bug with tests using koin.

--- network/src/test/java/test/kotlin/clean/ficiverson/cleansh/herolist/SuperHeroeRemoteDataSourceImplTest.kt
+++ network/src/test/java/test/kotlin/clean/ficiverson/cleansh/herolist/SuperHeroeRemoteDataSourceImplTest.kt
@@ -1,17 +1,33 @@
 package test.kotlin.clean.ficiverson.cleansh.herolist
 
 import org.assertj.core.api.Assertions
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import org.koin.standalone.StandAloneContext.closeKoin
+import org.koin.standalone.StandAloneContext.startKoin
+import org.koin.test.KoinTest
 import test.kotlin.clean.ficiverson.network.heroelist.SuperHeroeRemoteDataSourceImpl
+import test.kotlin.clean.ficiverson.network.injection.NetworkModules
 
 /**
  * Created by f.souto.gonzalez on 27/08/2018.
  */
-class SuperHeroeRemoteDataSourceImplTest {
+class SuperHeroeRemoteDataSourceImplTest : KoinTest {
+
+    @Before
+    fun before() {
+        startKoin(listOf(NetworkModules("https://api.myjson.com/").module))
+    }
+
+    @After
+    fun after() {
+        closeKoin()
+    }
 
     @Test
-    fun `that can handle a remote request`() {
+    fun `that we receive at least one superheroe`() {
         val superHeroeRemoteDataSource = SuperHeroeRemoteDataSourceImpl()
-        Assertions.assertThat(superHeroeRemoteDataSource.getAll()?.size).isEqualTo(0)
+        Assertions.assertThat(superHeroeRemoteDataSource.getAll()?.size).isGreaterThan(0)
     }
 }
