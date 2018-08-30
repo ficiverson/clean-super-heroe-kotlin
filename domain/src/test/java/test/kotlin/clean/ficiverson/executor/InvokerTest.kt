8f2e2fe3fed45c18c17849a07f44ea2dfe3f119b
fernandosouto <fernando.souto@mobgen.com>
2018-08-30 16:42:16 +0200

    Add test to domain layer

--- domain/src/test/java/test/kotlin/clean/ficiverson/executor/InvokerTest.kt
+++ domain/src/test/java/test/kotlin/clean/ficiverson/executor/InvokerTest.kt
@@ -1,8 +1,10 @@
 package test.kotlin.clean.ficiverson.executor
 
-import kotlinx.coroutines.experimental.launch
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import test.kotlin.clean.ficiverson.mock.instrumentation.InvokerInstruments.givenAnInvoker
+import test.kotlin.clean.ficiverson.mock.instrumentation.InvokerInstruments.givenAnInvokerAnCancelTasks
+import test.kotlin.clean.ficiverson.mock.instrumentation.UseCaseInstruments.givenAGenericSuccessResultUseCase
 
 /**
  * Created by f.souto.gonzalez on 27/08/2018.
@@ -11,37 +13,25 @@ class InvokerTest {
 
     @Test
     fun `that can create a use case invoker`() {
-        val invoker = object : Invoker {
-            override fun <P, T> execute(useCase: UseCase<P, T>, params: P, policy: CachePolicy, onResult: (Result<T>) -> Unit) {
-                launch {
-                    onResult(useCase.run(LocalOnly, params))
-                }
-            }
-
-            override fun cancelAllAsync() {
-                //Nothing to do
-            }
-
-            override fun isPendingTask(): Boolean {
-                return false
-            }
-        }
-        val useCase = object : UseCase<Unit, String> {
-            override suspend fun run(policy: CachePolicy, params: Unit): Result<String> = Success("Awesome Result")
-        }
+        val invoker = givenAnInvoker()
+        val useCase = givenAGenericSuccessResultUseCase()
         val params = Unit
 
-        invoker.execute(
-            useCase,
-            params,
-            LocalOnly,
-            ::onResult
-        )
-        //
+        invoker.execute(useCase, params, LocalOnly) {
+            assertThat(it is Success).isTrue()
+            assertThat((it as Success).data).isEqualTo("Awesome Result")
+        }
     }
 
-    fun onResult(result: Result<String>) {
-        assertThat(result is Success).isTrue()
-        assertThat((result as Success).data).isEqualTo("Awesome Result")
+    @Test
+    fun `that can cancel a job`() {
+        val invoker = givenAnInvokerAnCancelTasks()
+        val useCase = givenAGenericSuccessResultUseCase()
+        val params = Unit
+
+        invoker.execute(useCase, params, LocalOnly, {})
+        invoker.cancelAllAsync()
+        assertThat(invoker.isPendingTask()).isFalse()
     }
+
 }
